ebdbef6a16d59b1528b6b62c4cf94024e6ff3588
IgnacioSoto <isoto@columbushs.com>
2022-02-01 11:17:35 -0500

    save

--- unit10/src/com/columbushs/unit10/Unit10Main.java
+++ unit10/src/com/columbushs/unit10/Unit10Main.java
@@ -4,7 +4,15 @@ public class Unit10Main {
 
 	public static void main(String[] args) {
 		
-
+		Unit10_1Recursion recur = new Unit10_1Recursion();
+		recur.recursion1(4);
+		//recur.recursion2(4);
+		
+		//System.out.println(recur.recursion3(8));
+		
+		//System.out.println(recur.recursion4(4));
+		
+		//recur.recursion5("apple");
 	}
 
 }
--- unit10/src/com/columbushs/unit10/Unit10_1Recursion.java
+++ unit10/src/com/columbushs/unit10/Unit10_1Recursion.java
@@ -1,5 +1,45 @@
 package com.columbushs.unit10;
 
 public class Unit10_1Recursion {
+	
+	//A recursive method is one that calls itself.
+	//Similar to loops. 
+	
+	public void recursion1(int n) {
+		System.out.println(n);
+		if (n>2) {
+			recursion1(n-1);
+		}
+		System.out.println(n);
+	}
+	
+	public void recursion2(int n) {
+		System.out.println(n);
+		if (n>2) {
+			recursion1(n+1);
+		}
+		System.out.println(n);
+	}
+	
+	public int recursion3(int n) {
+		if (n==0) {
+			return 0;
+		}
+		return n + recursion3(n/2);
+	}
+	
+	public int recursion4(int n) {
+		if (n>0) {
+			return n+recursion4(n/2)+recursion4(n/3);
+		}
+		return 0;
+	}
+	
+	public void recursion5(String s) {
+		if (s.length()>1) {
+			recursion5(s.substring(2));
+			System.out.print(s.substring(0,1));
+		}
+	}
 
 }
